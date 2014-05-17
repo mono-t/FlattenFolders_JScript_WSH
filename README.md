7fc9744905a6fc629840e73e85933c776f6e5b0b
monotea (mono_t) <mono_try_and_error@yahoo.co.jp>
2014-05-18 00:48:10 +0900

    First commit
    最初のコミット。必要最低限の実行ファイル・スクリプト・READMEを追加

--- README.md
+++ README.md
@@ -1,4 +1,41 @@
-FlattenFolders_JScript_WSH
+WSHのJSでFF FlattenFolders_JScript_WSH
 ==========================
 
-空のフォルダの削除とサブフォルダが一つしかないフォルダを浅くするWSHのJScript
+空のフォルダの削除とサブフォルダが一つしかないフォルダを浅くするWSH(Windows Script Host)のJScript
+
+### このプロジェクトについて
+見ての通り、サンプルプログラムレベルの内容しかありません。
+かといって別の場所で作り直したりするのも面倒なので、せっかくだからリポジトリを作りました。
+
+### 機能・使い方
+このスクリプトとwsfファイルが置かれたフォルダの空のフォルダの削除とサブフォルダが一つしかないフォルダを浅くします。
+使用するときにはこの二ファイルをコピーしてください。
+Windowsならwsfファイルをダブルクリックか何かすれば動くんじゃないでしょうか。
+このFlattenは、一段階しか浅くしません。たとえばフォルダ構成が
+*hoge
+    * piyo
+        * foo
+            * bar1
+            * bar2
+だった場合、
+* piyo
+    * foo
+        * bar1
+        * bar2
+になります。
+消えたフォルダは戻ってこないので、気を付けてください。
+
+### 動作環境
+* Windows Script Hostの動作する環境
+
+ただし、作った人([@monotea_error](https://twitter.com/monotea_error))は Windows8.1Proでのみ動作確認を行っています。
+
+### 開発環境など
+Windows Script Hostで動作するJScriptをサクラエディタやTerapad、あるいはnotepadで編集しています。
+当初はTypeScriptを試しましたが、TypeScriptの恩恵を全くと言っていいほど得られなかったのでやめにしました。
+今後もしかしたらTypeScriptで作るように変わるかもしれません。
+
+#### ライセンス
+* MIT License
+
+MIT ライセンスの下で公開する、オープンソース / フリーソフトウェアです。一応。
---
+++ flatten.js
@@ -0,0 +1,52 @@
+﻿var scriptPath = WScript.ScriptFullName;
+scriptPath = scriptPath.substr(0, scriptPath.lastIndexOf(WScript.ScriptName));
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var folder = fso.GetFolder(scriptPath);
+
+var suffixOfTemp = "_TEMP_";
+
+//確認ダイアログ
+var wshShell = new ActiveXObject("WScript.Shell");
+var ans = wshShell.Popup(scriptPath + " において、空のフォルダの削除とサブフォルダ一つしかないフォルダを浅くする処理を行います", 0, "実行確認", 1 + 64);
+
+if( ans == 2 ) {
+    WScript.Quit(-1);
+}
+//本番
+var subFolderCollection = folder.SubFolders;
+var subfolder;
+
+var e = new Enumerator(subFolderCollection);
+
+for (; !e.atEnd(); e.moveNext()) {
+    subfolder = e.item();
+    if (subfolder.files.count == 0) {
+        if (subfolder.SubFolders.count == 0) {
+            subfolder.Delete();
+        } else if (subfolder.SubFolders.count == 1) {
+            var sube = new Enumerator(subfolder.SubFolders);
+            var subsubFolder = sube.item();
+            var sameName = false;
+            var movedFolderPath;
+            if (subfolder.Name != subsubFolder.Name) {
+                //
+            } else {
+                subsubFolder.Name = subsubFolder.Name + suffixOfTemp;
+                sameName = true;
+            }
+            movedFolderPath = scriptPath + subsubFolder.Name;
+           // WScript.Echo("処理 : " + movedFolderPath);
+            subsubFolder.Move(scriptPath);
+            if (subfolder.SubFolders.count == 0) {
+                var tempPath = subfolder.Path + subfolder.Name;
+                subfolder.Delete();
+            } else {
+                WScript.Echo("Error");
+                WScript.Quit(-1);
+            }
+        } else {
+            WScript.Echo("複数のサブフォルダ : " + subfolder.Name);
+        }
+    }
+}
+WScript.Echo("処理が終了しました");
